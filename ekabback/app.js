af88a4d010cba506083802ffe2139ac84699db97
Bendali Safarli <bsafarli@icloud.com>
2021-02-21 03:55:37 +0400

    application code updated

--- ekabback/app.js
+++ ekabback/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const dbconn = require("./dbconn.js")
 const { response } = require("express")
 
 const app = express()
@@ -15,5 +16,11 @@ app.use(
 app.get('/', (req, res) => {
     res.json({ info: 'From nodejs express server!' })
 })
+
+app.get('/users', dbconn.getUsers)
+app.get('/users/:id', dbconn.getUserById)
+app.post('/users', dbconn.createUser)
+app.put('/users/:id', dbconn.updateUser)
+app.delete('/users/:id', dbconn.deleteUser)
 
 app.listen(port, () => console.log('server has started'))
