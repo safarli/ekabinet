8d82037e26738ad6aaaf7d0bf439e26829b38e94
Ofis PC <binnacles@gmail.com>
2021-02-24 15:55:19 +0400

    frontend update

--- ekabfront/src/main.js
+++ ekabfront/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+// import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+Vue.createApp(App).mount('#app')
